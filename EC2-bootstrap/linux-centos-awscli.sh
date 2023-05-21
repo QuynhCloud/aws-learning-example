6155803b68b47118b2cc1ad548c15c811298c184
QuynhCloud <134202724+QuynhCloud@users.noreply.github.com>
2023-05-21 19:01:24 +0700

    Update linux-centos-awscli.sh

--- EC2-bootstrap/linux-centos-awscli.sh
+++ EC2-bootstrap/linux-centos-awscli.sh
@@ -13,21 +13,22 @@ echo "<html>" > index.html
 METADATA='http://169.254.169.254'
 
 echo "<h1>Welcome to EC2</h1>" >> index.html
-
-instance_id=$(curl -s http://169.254.169.254/latest/meta-data/instance-id)
-export AWS_DEFAULT_REGION="Your AWS Region"
-/usr/bin/aws ec2 create-tags --resources $instance_id --tags'Key="abcde",Value=12345'`
-
 echo "<h4>You are running instance from this IP (This is for testing purpose only, you should not public this to user):</h4>"
 status_code=$(curl -s -o /dev/null -w "%{http_code}" $METADATA/latest/meta-data/)
+instance_id=$(curl -s http://169.254.169.254/latest/meta-data/instance-id)
+
 if [[ "$status_code" -eq 200 ]]
 then
     export privateIp=`curl $METADATA/latest/meta-data/local-ipv4`
     export publicIP=`curl $METADATA/latest/meta-data/public-ipv4`
+    export AWS_DEFAULT_REGION="Your AWS Region"
+                                /usr/bin/aws ec2 create-tags --resources $instance_id --tags'Key="abcde",Value=12345'`
 else
     TOKEN=`curl -X PUT "$METADATA/latest/api/token" -H "X-aws-ec2-metadata-token-ttl-seconds: 21600"`
     export privateIp=`curl -H "X-aws-ec2-metadata-token: $TOKEN" -v $METADATA/latest/meta-data/local-ipv4`
     export publicIP=`curl -H "X-aws-ec2-metadata-token: $TOKEN" -v $METADATA/latest/meta-data/public-ipv4`
+    export AWS_DEFAULT_REGION="Your AWS Region"
+                                /usr/bin/aws ec2 create-tags --resources $instance_id --tags'Key="abcde",Value=12345'`
 fi   
 echo "<br>Private IP: " >> index.html
 echo $privateIp >> index.html
